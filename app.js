0438fc2ee4d81422170b97f7101855f53493d543
m.zedan <m.zedan@apex.ps>
2025-04-14 18:08:25 +0200

    database create

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
@@ -11,3 +12,5 @@ app.get('/' , (req , res) => {
 app.listen(port , () => {
     console.log(`Example app listening on port ${port} on server http://localhost:${port}/`)
 })  
+
+mongoose.connect('mongodb+srv://raidenzed:zx2855776655@cluster0.j2apcfi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {}).catch(err => console.log(err));
